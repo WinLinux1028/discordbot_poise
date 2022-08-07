7b276016897f8521fdc57254cdd8cd23628a8736
SovietReimu1228 <reimussr@protonmail.ch>
2022-08-08 00:38:53 +0900

    1プロセスで2つのBOTを動かす機能が正常に動かないバグを修正

--- src/main.rs
+++ src/main.rs
@@ -23,31 +23,37 @@ async fn main() {
     let config: Config = serde_json::from_str(&config).unwrap();
 
     for data in config.0 {
-        let options = poise::FrameworkOptions {
-            on_error: |err| Box::pin(error::on_error(err)),
-            commands: vec![
-                owner::register(),
-                general::help(),
-                general::ping(),
-                general::say(),
-                general::nade(),
-            ],
-            listener: |ctx, event, fwctx, data| {
-                Box::pin(listener::listener(ctx, event, fwctx, data))
-            },
-            ..Default::default()
-        };
-
-        let framework = poise::Framework::builder()
-            .options(options)
-            .token(&data.token)
-            .intents(serenity::GatewayIntents::all())
-            .user_data_setup(|ctx, ready, framework| {
-                Box::pin(ready::ready(ctx, ready, framework, data))
-            });
-
-        framework.run_autosharded().await.unwrap();
+        tokio::spawn(new_bot(data));
     }
+
+    loop {
+        tokio::time::sleep(std::time::Duration::from_secs(u64::MAX)).await;
+    }
+}
+
+async fn new_bot(data: Data) {
+    let options = poise::FrameworkOptions {
+        on_error: |err| Box::pin(error::on_error(err)),
+        commands: vec![
+            owner::register(),
+            general::help(),
+            general::ping(),
+            general::say(),
+            general::nade(),
+        ],
+        listener: |ctx, event, fwctx, data| Box::pin(listener::listener(ctx, event, fwctx, data)),
+        ..Default::default()
+    };
+
+    let framework = poise::Framework::builder()
+        .options(options)
+        .token(&data.token)
+        .intents(serenity::GatewayIntents::all())
+        .user_data_setup(|ctx, ready, framework| {
+            Box::pin(ready::ready(ctx, ready, framework, data))
+        });
+
+    framework.run_autosharded().await.unwrap();
 }
 
 #[derive(serde::Serialize, serde::Deserialize)]
