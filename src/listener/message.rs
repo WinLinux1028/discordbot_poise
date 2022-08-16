d024668808b8b6a9f207e5f88991425fc911d0b3
SovietReimu1228 <reimussr@protonmail.ch>
2022-08-16 18:27:55 +0900

    細かな調整

--- src/listener/message.rs
+++ src/listener/message.rs
@@ -4,16 +4,12 @@ use poise::serenity_prelude as serenity;
 
 impl Listener<'_> {
     pub async fn message(self, new_message: &serenity::Message) {
-        if new_message.author.bot {
+        if new_message.author.bot || self.data.is_muted(new_message.author.id).await {
             return;
         }
 
         if let Some(globalchat) = &self.data.globalchat {
-            if self.data.is_muted(new_message.author.id).await {
-                let _ = new_message.delete(self.ctx).await;
-            } else {
-                let _ = globalchat.send_msg(self.ctx, new_message).await;
-            }
+            let _ = globalchat.send_msg(self.ctx, new_message).await;
         }
     }
 }
